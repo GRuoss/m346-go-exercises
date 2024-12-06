4035b60f92a431caffb5dcc45e310df65fad680e
Gregory Ruoss <Gregory_Ruoss@sluz.ch>
2024-12-06 14:03:52 +0100

    Exersise 1 completed

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	firstName := "Gregory"
+	lastName := "Ruoss"
+	dayOfBirth := 13
+	monthOfBirth := 11
+	yearOfBirth := 2007
+	numberOfSiblings := 2
+	heightInMeters := 1.80
+	zodiacSign := '\u264E'
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
