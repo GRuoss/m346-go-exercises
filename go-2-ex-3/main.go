eaf1e28bb24783f0b1ce76ff71bd694e848d00e2
Gregory Ruoss <Gregory_Ruoss@sluz.ch>
2024-12-06 15:27:18 +0100

    go-2-ex-3 finished

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+	modules := make(map[int]uint, 0)
+	
+	modules[104] = 3 
+	modules[117] = 4 
+	modules[346] = 5
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
-	fmt.Println(modules)
+	delete(modules, 104)
+	fmt.Println("Nach Löschen von Modul 104:", modules)
+
+	modules[215] = 6 
+	fmt.Println("Nach Hinzufügen von Modul 215:", modules)
+
+	modules[117] = 7 
+	fmt.Println("Nach Ersetzen von Modul 117:", modules)
 }
