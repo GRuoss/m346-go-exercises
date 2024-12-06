532334caf8b0bf8f788e8feee0e229071d81e15e
Gregory Ruoss <Gregory_Ruoss@sluz.ch>
2024-12-06 14:42:16 +0100

    Forgot to commit did all go1 exersises

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -4,17 +4,28 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"os"
 )
 
 func main() {
+
 	var eyes = rand.Intn(5) + 1
 	var when = time.Now()
+eyesFile, err:= os.Create("eyes.txt") 
+if err != nil {
+	fmt.Println("Error creating eyes.txt", err)
+    return
+}
+logDice, err := os.Create("dice.log")
+if err != nil {
+	fmt.Println("Error creating dice.log", err)
+    return
+}
+	// TODO: use fmt.Fprintln instead!
+	fmt.Fprintln(eyesFile,"the dice shows", eyes, "eyes")
 
 	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
-
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	fmt.Fprintln(logDice,"the dice was rolled at", when)
 
 	// TODO: how to write the output into eyes.txt and dice.log?
 	// go run ex3/main.go TODO
