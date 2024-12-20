aeb94bf2f18ad9e6b3ee8c8f08eb92b51e2035e4
Gregory Ruoss <Gregory_Ruoss@sluz.ch>
2024-12-20 14:42:51 +0100

    go-4-ex-3 done

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,45 @@
 package main
 
-// TODO: implement the function computeQuadraticFormula
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func computeQuadraticFormula(a float64, b float64, c float64) ([]float64, error) {
+	D := math.Pow(b, 2) - 4*a*c
+	if D < 0 {
+		return nil, errors.New("error: D < 0")
+	} else if D == 0 {
+		x := (-b + math.Sqrt(D)) / (2 * a)
+		return []float64{x}, nil
+	} else {
+		x1 := (-b + math.Sqrt(D)) / (2 * a)
+		x2 := (-b - math.Sqrt(D)) / (2 * a)
+		return []float64{x1, x2}, nil
+	}
+}
 
 func main() {
-	// TODO: call the function computeQuadraticFormula
+	
+	solutions, err := computeQuadraticFormula(1.0, -3.0, 2.0)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Lösungen für a=1, b=-3, c=2:", solutions)
+	}
+
+	solutions, err = computeQuadraticFormula(1.0, 2.0, 1.0)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Lösungen für a=1, b=2, c=1:", solutions) 
+	}
+
+	solutions, err = computeQuadraticFormula(1.0, 1.0, 1.0)
+	if err != nil {
+		fmt.Println(err) 
+	} else {
+		fmt.Println("Lösungen für a=1, b=1, c=1:", solutions)
+	}
 }
