c3dd6f6e24cb5a85517853029c00bdf971e81f03
Gregory Ruoss <Gregory_Ruoss@sluz.ch>
2024-12-20 14:48:27 +0100

    go-4-ex-4

--- go-4-ex-4/main.go
+++ go-4-ex-4/main.go
@@ -1,9 +1,20 @@
 package main
 
-// TODO: implement the function convertCelsiusToFahrenheit
-// TODO: implement the function convertFahrenheitToCelsius
 
+import (
+	"fmt"
+)
+// TODO: implement the function convertCelsiusToFahrenheit
+func convertCelsiusToFahrenheit(celsius float64) float64 {
+	F:= (celsius * 9/5) + 32
+	return F
+}
+// TODO: implement the function convertFahrenheitToCelsius
+func convertFahrenheitToCelsius(fahrenheit float64) float64 {
+	C:= (fahrenheit - 32) * 5/9
+	return C
+}
 func main() {
-	// TODO: call the function convertCelsiusToFahrenheit
-	// TODO: call the function convertFahrenheitToCelsius
+	fmt.Println(convertCelsiusToFahrenheit(10)) // resultate = 50 
+	fmt.Println(convertFahrenheitToCelsius(10)) // resultate = -12.222222222222221
 }
