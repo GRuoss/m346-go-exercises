aef3ae804d349a8c4328d67bb58a9b472d6a7a07
Gregory Ruoss <Gregory_Ruoss@sluz.ch>
2024-12-20 14:25:04 +0100

    go-4-ex-2 done

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,13 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import "math"
 
+// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+func computeHypotenuse(a float64, b float64) float64 {
+	c := math.Sqrt( math.Pow(a, 2) + math.Pow(b, 2))
+	return c
+}
 func main() {
 	// TODO: call the function computeHypotenuse
+	computeHypotenuse(3.0, 4.0)
 }
